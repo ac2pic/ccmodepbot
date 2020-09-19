004791fa4f9d89c783962c0f91924eadf28b37f4
ac2pic <ac2pic@gmail.com>
2020-09-19 14:23:34 -0500

    Added tests for mod dependency details

--- test/dependency-checker.test.ts
+++ test/dependency-checker.test.ts
@@ -36,3 +36,42 @@ test('if dependency is newer than database provided version returns nothing', ()
     expect(DependencyChecker.findOutdated(mockDependencyList, mockLatestVersions))
         .toStrictEqual({});
 })
+
+test('if optional dependencies retain form if update necessary', () => {
+    const mockDependencyList = {
+        "my-mod": {
+            "version": "1.0.6",
+            "optional": true
+        }
+    };
+
+    const mockLatestVersions = {
+        "my-mod": "1.0.7"
+    };
+
+    expect(DependencyChecker.findOutdated(mockDependencyList, mockLatestVersions))
+        .toStrictEqual({
+            "my-mod": {
+                "version": "1.0.7",
+                "optional": true
+            }
+        }); 
+})
+
+test('if converts ModDependencyDetails to string if updated dependency is necessary', () => {
+    const mockDependencyList = {
+        "my-mod": {
+            "version": "1.0.6",
+            "optional": false
+        }
+    };
+
+    const mockLatestVersions = {
+        "my-mod": "1.0.7"
+    };
+
+    expect(DependencyChecker.findOutdated(mockDependencyList, mockLatestVersions))
+        .toStrictEqual({
+            "my-mod": "1.0.7"
+        }); 
+})
